fe1dd4d53b698f0231f5b83416e4111e7446f4f5
Mustafa <mmermmus123@gmail.com>
2023-04-03 03:33:26 +0300

    update

---
+++ analyzer/src/analyzer/Program.java
@@ -0,0 +1,17 @@
+package analyzer;
+
+import java.io.IOException;
+
+import test.Test;
+import util.Todo;
+
+@Todo("todo")
+
+public class Program {
+
+	public static void main(String[] args) throws IOException {
+		Test t = new Test();
+
+	}
+
+}
