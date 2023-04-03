347371a6aaf2895f010bbf0e30293cb5304122a6
Mustafa <mmermmus123@gmail.com>
2023-04-03 16:46:55 +0300

    method and constructor dedect patterns added.

--- analyzer/src/test/GetPatern.java
+++ analyzer/src/test/GetPatern.java
@@ -3,6 +3,8 @@ package test;
 import java.util.regex.Pattern;
 
 public class GetPatern {
+	final static String className = "Test";
+
 	public static Pattern getPatern(Paterns patern) {
 		switch (patern) {
 		case single:
@@ -11,6 +13,10 @@ public class GetPatern {
 			return Pattern.compile("/\\*(?!\\*).{2}.*?\\*/", Pattern.DOTALL);
 		case javadoc:
 			return Pattern.compile("/\\*\\*([^*]|[\\r\\n]|(\\*+([^*/]|[\\r\\n])))*\\*+/");
+		case method:
+			return Pattern.compile("(public|private|protected)?\\s+static?\\s+\\w+\\s+(\\w+)\\s*\\([^\\)]*\\)\\s*\\{");
+		case constructor:
+			return Pattern.compile("(public|protected|private)\\s+" + className + "\\s*\\(.*?\\)\\s*\\{");
 		default:
 			return Pattern.compile("//.*$");
 		}
