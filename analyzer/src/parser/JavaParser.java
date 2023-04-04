7e9c9ba23d5292624edc47c15dea25fbdf322f8e
Mustafa <mmermmus123@gmail.com>
2023-04-04 19:35:15 +0300

    update

--- analyzer/src/parser/JavaParser.java
+++ analyzer/src/parser/JavaParser.java
@@ -1,5 +1,13 @@
 package parser;
 
-public class JavaParser {
+import parser.classes.AClassParser;
+import parser.comment.ACommentParser;
+import parser.function.AFunctionParser;
+
+public class JavaParser extends AParser {
+
+	public JavaParser(AFunctionParser func, ACommentParser comm, AClassParser clss) {
+		super(func, comm, clss);
+	}
 
 }
