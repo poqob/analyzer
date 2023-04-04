046542ff10948d52523e4892f5d8204979a9b8b5
Mustafa <mmermmus123@gmail.com>
2023-04-05 01:42:30 +0300

    names changed

---
+++ analyzer/src/util/Debug.java
@@ -0,0 +1,16 @@
+package util;
+
+public class Debug {
+
+	public static void Writer(String message) {
+		System.out.print(message);
+	}
+
+	public static void Writer(Long message) {
+		System.out.print(message);
+	}
+
+	public static void Writer(int message) {
+		System.out.print(message);
+	}
+}
