0414a2f3036f1f5cd5e346512600aa631ef05c37
Mustafa <mmermmus123@gmail.com>
2023-04-05 22:39:11 +0300

    java comment parser(returns AComment list) class done. tests done.

--- analyzer/src/parser/comment/JavaCommentParser.java
+++ analyzer/src/parser/comment/JavaCommentParser.java
@@ -1,5 +1,105 @@
 package parser.comment;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import models.classes.JavaClass;
+import models.comment.AComment;
+import models.comment.JavaMultiComment;
+import models.comment.JavaSingleComment;
+import models.comment.JavadocComment;
+import parser.patterns.java.GetPatern;
+import parser.patterns.java.Paterns;
+
 public class JavaCommentParser extends ACommentParser {
+
+	public JavaCommentParser(JavaClass clss) {
+		super(clss);
+	}
+
+	private void _find() {
+
+		// single comments
+		_single();
+		_multiComment();
+		_javadocComment();
+
+	};
+
+	// turn class into bufferedReader inorder to read line by line.
+	// line by line is neccessary to dedect single line comments.
+	private void _single() {
+		String str = super.clss.toString();
+		BufferedReader reader = new BufferedReader(new StringReader(str));
+		String line;
+		try {
+			while ((line = reader.readLine()) != null) {
+				// send lines to single line comment dedector.
+				_singleComment(line);
+			}
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+	// dedects single line comments
+	private void _singleComment(String line) {
+
+		Paterns p = Paterns.single;
+		Pattern pa = GetPatern.getPatern(p);
+		Matcher matcher = pa.matcher(line);
+
+		int _startIndex = 0;
+		String match = "";
+
+		while (matcher.find()) {
+			match = matcher.group();
+			_startIndex = super.clss.toString().indexOf(match);
+			super.comments.add(new JavaSingleComment(new int[] { _startIndex, match.length() + _startIndex }, match));
+		}
+
+	}
+
+	// dedects multi line java comments
+	private void _multiComment() {
+		Paterns p = Paterns.multi;
+		Pattern pa = GetPatern.getPatern(p);
+		Matcher matcher = pa.matcher(clss.toString());
+		int _startIndex = 0;
+		String match = "";
+
+		while (matcher.find()) {
+			match = matcher.group();
+			_startIndex = super.clss.toString().indexOf(match);
+			super.comments.add(new JavaMultiComment(new int[] { _startIndex, match.length() + _startIndex }, match));
+		}
+	}
+
+	// dedects javadoc comments
+	private void _javadocComment() {
+		Paterns p = Paterns.javadoc;
+		Pattern pa = GetPatern.getPatern(p);
+		Matcher matcher = pa.matcher(clss.toString());
+		int _startIndex = 0;
+		String match = "";
+
+		while (matcher.find()) {
+			match = matcher.group();
+			_startIndex = super.clss.toString().indexOf(match);
+			super.comments.add(new JavadocComment(new int[] { _startIndex, match.length() + _startIndex }, match));
+		}
+	}
+
+	@Override
+	public List<AComment> parse() {
+		_find();
+		return super.comments;
+	}
 
 }
