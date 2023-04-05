ab4f31a41cbbd5710bdeb51fd95194a2af89b4e5
Mustafa <mmermmus123@gmail.com>
2023-04-06 01:37:34 +0300

    name changed, test file updated

--- analyzer/src/models/functionAndComments/java/JavaConstructorWithComments.java
+++ analyzer/src/models/functionAndComments/java/JavaConstructorWithComments.java
@@ -1,9 +1,10 @@
-package models.functionAndComments;
+package models.functionAndComments.java;
 
 import models.function.AFunction;
+import models.functionAndComments.AFunctionWithComments;
 
-public class ConstructorWithComments extends AFunctionWithComments {
-	public ConstructorWithComments(AFunction function) {
+public class JavaConstructorWithComments extends AFunctionWithComments {
+	public JavaConstructorWithComments(AFunction function) {
 		super(function);
 	}
 
