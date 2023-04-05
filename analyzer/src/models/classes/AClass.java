f2df7898354a6375caf15d387dd8c3ff8ca86f24
Mustafa <mmermmus123@gmail.com>
2023-04-05 22:35:20 +0300

    classes has new methods and fields that are related with funComms

--- analyzer/src/models/classes/AClass.java
+++ analyzer/src/models/classes/AClass.java
@@ -2,19 +2,42 @@ package models.classes;
 
 import java.util.ArrayList;
 
+import models.comment.AComment;
+import models.function.AFunction;
 import models.functionAndComments.AFunctionWithComments;
 
 public abstract class AClass {
 	protected final String name;
 	protected final String text;
-	protected ArrayList<AFunctionWithComments> processes;
+
+	private ArrayList<AFunction> functions;
+	private ArrayList<AComment> comments;
+	private ArrayList<AFunctionWithComments> funcoms;
 
 	public AClass(String name, String text) {
 		this.name = name;
-
 		this.text = text;
-		processes = new ArrayList<AFunctionWithComments>();
 	};
+
+	public void setFunctions(ArrayList<AFunction> functions) {
+		this.functions = functions;
+	}
+
+	public void setComments(ArrayList<AComment> comments) {
+		this.comments = comments;
+	}
+
+	public ArrayList<AFunction> getFunctions() {
+		return functions;
+	}
+
+	public ArrayList<AComment> getComments() {
+		return comments;
+	}
+
+	public ArrayList<AFunctionWithComments> getFunComs() {
+		return funcoms;// null
+	}
 
 	public String getName() {
 		return name.trim();
