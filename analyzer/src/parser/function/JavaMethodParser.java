629f798aa6aa488005854e467d07e2f958618db7
Mustafa <mmermmus123@gmail.com>
2023-04-05 13:57:45 +0300

    method parser updated according to new method pattern

--- analyzer/src/parser/function/JavaMethodParser.java
+++ analyzer/src/parser/function/JavaMethodParser.java
@@ -4,13 +4,12 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import models.classes.AClass;
+import models.classes.JavaClass;
 import models.function.AFunction;
 import models.function.Method;
 import parser.patterns.java.GetPatern;
 import parser.patterns.java.Paterns;
 import parser.util.java.CurlyBraces;
-import util.Debug;
 import util.Todo;
 
 @Todo("method parser only parses first method of class (non constructor)")
@@ -18,7 +17,7 @@ public class JavaMethodParser extends AFunctionParser {
 
 	private String _head;
 
-	public JavaMethodParser(AClass clss) {
+	public JavaMethodParser(JavaClass clss) {
 		super(clss);
 
 		super.funList = new ArrayList<AFunction>();
@@ -36,15 +35,18 @@ public class JavaMethodParser extends AFunctionParser {
 		// loop
 		while (matcher.find()) {
 			// attemt head
-			_head = matcher.group();
+			this._head = matcher.group();
 			_headIndex = clss.toString().indexOf(_head);
+
+			if (super.clss.getName().contains(_getName()))
+				continue;
 			// find body
 			cb = new CurlyBraces(clss.toString().substring(_headIndex));
 
 			// range
 			range = new int[] { _headIndex, cb.getBody().length() + _headIndex };
 
-			Debug.Writer(_getName() + "\n");
+			// Debug.Writer(_getName() + "\n");
 			// attemt to funList as new created AFunction
 			super.funList.add(new Method(_getName(), range, _getHead() + cb.getBody()));
 
@@ -59,6 +61,7 @@ public class JavaMethodParser extends AFunctionParser {
 	// returns method's name
 	private String _getName() {
 		String head = _head;
+
 		int _start = 0;
 		int _stop = head.indexOf("()");
 		for (int i = _stop; i > 0; i--) {
@@ -66,6 +69,7 @@ public class JavaMethodParser extends AFunctionParser {
 			if (head.charAt(i) == ' ')
 				break;
 		}
+		// Debug.Writer(head);
 
 		return head.substring(_start, _stop).trim();
 	}
