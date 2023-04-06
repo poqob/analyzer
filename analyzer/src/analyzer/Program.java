e4c23b4494617782f5cd1f3d6afbc0dd0d223ffd
Mustafa <mmermmus123@gmail.com>
2023-04-07 00:18:19 +0300

    output path argument added. the path is local execute path as default.

--- analyzer/src/analyzer/Program.java
+++ analyzer/src/analyzer/Program.java
@@ -10,13 +10,15 @@ import writer.javaWriter.JavaWriter;
 
 @Todo("if a real function have commeted and located in a comment but classified as a function!!!!")
 
-//java -jar test.jar C:\Users\mmerm\git\repository\analyzer\src\test\test.txt arg1=write destinition_path default= local
+//java -jar test.jar test.java ..\\output\\
+//java -jar test.jar argument0 argument1=local folder path
+
 public class Program {
 
-	@SuppressWarnings("unused")
 	public static void main(String[] args) throws IOException {
 		Scanner scanner = new Scanner(System.in);
 
+		// controlling arguments
 		if (args.length == 0)
 			System.out.println("No arguments provided.");
 		else {
@@ -24,11 +26,19 @@ public class Program {
 			JavaClass clss = parser.parse();
 			JavaWriter writer = new JavaWriter(clss);
 
+			// this method prints the requested(by homework) output to the screen
 			writer.writeToConsole();
-			writer.writeToFile();
 
+			// if second argumend provided by user, run write to file with specified path
+			// else create files into execute path.
+			if (args.length == 2)
+				writer.writeToFile(args[1]);
+			else
+				writer.writeToFile();
 		}
+		System.out.println("press any key to exit.\n");
 		scanner.nextLine();
+
 		scanner.close();
 	}
 
