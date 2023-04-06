d3883f10083d7d88a7da9a4c6567b31687ecd8d2
Mustafa <mmermmus123@gmail.com>
2023-04-07 02:14:39 +0300

    AMultiComment was documanted.

--- analyzer/src/models/comment/AMultiComment.java
+++ analyzer/src/models/comment/AMultiComment.java
@@ -1,7 +1,17 @@
+/**
+*
+* @author Mustafa BÝÇER, mustafa.bicer1@ogr.sakarya.edu.tr
+* @since 05.04.23
+* @AMultiComment
+* AMultiComment is a abstract model for document style comments which derived from AComment. 
+* it works exactly same with AComment. Difference is it has type.
+*/
+
 package models.comment;
 
 public abstract class AMultiComment extends AComment {
 
+	// constructor
 	public AMultiComment(int[] range, String content, String type) {
 		super(range, content, type);
 	}
