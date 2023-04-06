267bf3bcba6da86d8ae14c2ae27dc139706d38bb
Mustafa <mmermmus123@gmail.com>
2023-04-07 02:09:23 +0300

    AComment was documanted.

--- analyzer/src/models/comment/AComment.java
+++ analyzer/src/models/comment/AComment.java
@@ -1,8 +1,19 @@
+/**
+*
+* @author Mustafa BÝÇER, mustafa.bicer1@ogr.sakarya.edu.tr
+* @since 05.04.23
+* @AComment
+* AComment is a abstract model for comments. it stores comment's itself
+* comment type (single-multi-special) and comment's bound in class.
+*/
+
 package models.comment;
 
+// range: comment's bound in class. {startIndex,stopIndex}
+//if you take substring of the class according to the range, you can get comment.
 public abstract class AComment {
 	protected final int[] range;
-	protected final String type;
+	protected final String type;// single-multi-specified
 	protected final String content;
 
 	public AComment(int[] range, String content, String type) {
@@ -11,6 +22,7 @@ public abstract class AComment {
 		this.type = type;
 	}
 
+	// getters
 	public int[] getRange() {
 		return range;
 	};
