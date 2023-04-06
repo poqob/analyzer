e2c8b063fd4ea83e7193640e22bb383fdd52a288
Mustafa <mmermmus123@gmail.com>
2023-04-07 02:13:39 +0300

    ADocumentComment was documanted.

--- analyzer/src/models/comment/ADocumentComment.java
+++ analyzer/src/models/comment/ADocumentComment.java
@@ -1,7 +1,17 @@
+/**
+*
+* @author Mustafa BÝÇER, mustafa.bicer1@ogr.sakarya.edu.tr
+* @since 05.04.23
+* @ADocumentComment
+* ADocumentComment is a abstract model for document style comments which derived from AComment. 
+* it works exactly same with AComment. Difference is it has type.
+*/
+
 package models.comment;
 
 public abstract class ADocumentComment extends AComment {
 
+	// constructor
 	public ADocumentComment(int[] range, String content, String type) {
 		super(range, content, type);
 	}
