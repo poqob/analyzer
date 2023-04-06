80fac67a6845c107fa84546e129ba1e0c0a9becc
Mustafa <mmermmus123@gmail.com>
2023-04-06 21:28:41 +0300

    minor warnings fixed + first test.jar, it works!

--- analyzer/src/analyzer/Program.java
+++ analyzer/src/analyzer/Program.java
@@ -1,19 +1,31 @@
 package analyzer;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import test.Test;
 import util.Todo;
 
-@Todo("a screen printer and file system manager will be coded."
-		+ " in addition to that, our parser cant controll the situation that is "
-		+ "if a real function have commeted and located in a comment but classified as a function!!!!")
+@Todo("if a real function have commeted and located in a comment but classified as a function!!!!")
 
+//java -jar test.jar C:\Users\mmerm\git\repository\analyzer\src\test\test.txt
 public class Program {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) throws IOException {
-		Test t = new Test();
+		Scanner scanner = new Scanner(System.in);
+		Test t;
 
+		// args[0] = "C:\\Users\\mmerm\\git\\repository\\analyzer\\src\\test\\test.txt";
+
+		if (args.length == 0)
+			System.out.println("No arguments provided.");
+		else {
+			t = new Test(args[0]);
+
+		}
+		scanner.nextLine();
+		scanner.close();
 	}
 
 }
--- analyzer/src/test/Test.java
+++ analyzer/src/test/Test.java
@@ -1,15 +1,16 @@
 package test;
 
-import analyzer.writer.java.JavaWriter;
 import models.classes.java.JavaClass;
 import parser.java.JavaParser;
 import util.Debug;
+import writer.javaWriter.JavaWriter;
 
 public class Test {
 
-	public Test() {
+	public Test(String path) {
 
-		String path = "C:\\Users\\mmerm\\git\\repository\\analyzer\\src\\test\\test.txt";
+		// String path =
+		// "C:\\Users\\mmerm\\git\\repository\\analyzer\\src\\test\\test.txt";
 		JavaParser parser = new JavaParser(path);
 		JavaClass clss = parser.parse();
 		JavaWriter writer = new JavaWriter(clss);
