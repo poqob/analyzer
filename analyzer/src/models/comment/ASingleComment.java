8ec8c2a51cf12e0cd1e3d52ec6a3621bdf7bb7ee
Mustafa <mmermmus123@gmail.com>
2023-04-07 02:15:28 +0300

    ASingleComment was documanted.

--- analyzer/src/models/comment/ASingleComment.java
+++ analyzer/src/models/comment/ASingleComment.java
@@ -1,7 +1,17 @@
+/**
+*
+* @author Mustafa BÝÇER, mustafa.bicer1@ogr.sakarya.edu.tr
+* @since 05.04.23
+* @ASingleComment
+* ASingleComment is a abstract model for document style comments which derived from AComment. 
+* it works exactly same with AComment. Difference is it has type.
+*/
+
 package models.comment;
 
 public abstract class ASingleComment extends AComment {
 
+	// constructor
 	public ASingleComment(int[] range, String content, String type) {
 		super(range, content, type);
 	}
