9272b7a37fafc2e688e99a36b6281f48ee67b7e5
Mustafa <mmermmus123@gmail.com>
2023-04-07 04:11:06 +0300

    java functions were documanted.

--- analyzer/src/models/functionAndComments/java/JavaConstructorWithComments.java
+++ analyzer/src/models/functionAndComments/java/JavaConstructorWithComments.java
@@ -1,8 +1,18 @@
+/**
+*
+* @author Mustafa BÝÇER, mustafa.bicer1@ogr.sakarya.edu.tr
+* @since 06.04.23
+* @JavaConstructorWithComments
+* JavaConstructorWithComments is a sub type of AFunctionWithComments.
+* it stores a function(constructor) with its comments.
+*/
+
 package models.functionAndComments.java;
 
 import models.function.AFunction;
 import models.functionAndComments.AFunctionWithComments;
 
+//constructor
 public class JavaConstructorWithComments extends AFunctionWithComments {
 	public JavaConstructorWithComments(AFunction function) {
 		super(function);
--- analyzer/src/models/functionAndComments/java/JavaMethodWithComments.java
+++ analyzer/src/models/functionAndComments/java/JavaMethodWithComments.java
@@ -1,10 +1,19 @@
+/**
+*
+* @author Mustafa BÝÇER, mustafa.bicer1@ogr.sakarya.edu.tr
+* @since 06.04.23
+* @JavaMethodWithComments
+* JavaMethodWithComments is a sub type of AFunctionWithComments.
+* it stores a function(method) with its comments.
+*/
+
 package models.functionAndComments.java;
 
 import models.function.AFunction;
 import models.functionAndComments.AFunctionWithComments;
 
 public class JavaMethodWithComments extends AFunctionWithComments {
-
+	// constructor
 	public JavaMethodWithComments(AFunction function) {
 		super(function);
 	}
