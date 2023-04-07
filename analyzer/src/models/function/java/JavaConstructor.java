83fa71f1ad0e4e7ed7995995b654f2952030f5c4
Mustafa <mmermmus123@gmail.com>
2023-04-07 03:57:19 +0300

    java method and constructor classes were documanted.

--- analyzer/src/models/function/java/JavaConstructor.java
+++ analyzer/src/models/function/java/JavaConstructor.java
@@ -1,9 +1,18 @@
+/**
+*
+* @author Mustafa BÝÇER, mustafa.bicer1@ogr.sakarya.edu.tr
+* @since 06.04.23
+* @JavaConstructor
+* JavaConstructor is a model for java constructors. it is derived from 
+* AFunction and works exactly same.
+*/
 package models.function.java;
 
 import models.function.AFunction;
 
 public class JavaConstructor extends AFunction {
 
+	// constructor
 	public JavaConstructor(String name, int[] range, String content) {
 		super(name, range, content);
 		super.setType("CONSTRUCTOR");
--- analyzer/src/models/function/java/JavaMethod.java
+++ analyzer/src/models/function/java/JavaMethod.java
@@ -1,8 +1,17 @@
+/**
+*
+* @author Mustafa BÝÇER, mustafa.bicer1@ogr.sakarya.edu.tr
+* @since 06.04.23
+* @JavaMethod
+* JavaMethod is a model for java method. it is derived from 
+* AFunction and works exactly same.
+*/
 package models.function.java;
 
 import models.function.AFunction;
 
 public class JavaMethod extends AFunction {
+	// constructor
 	public JavaMethod(String name, int[] range, String content) {
 		super(name, range, content);
 		super.setType("METHOD");
