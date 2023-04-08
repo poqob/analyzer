ea4c60fa0b0c50e4c2db50d154b71ae2437aeda9
Mustafa <mmermmus123@gmail.com>
2023-04-08 06:33:16 +0300

    Messager class was documanted.

--- analyzer/src/handler/messager/Messager.java
+++ analyzer/src/handler/messager/Messager.java
@@ -1,3 +1,10 @@
+/**
+*
+* @author Mustafa BÝÇER, mustafa.bicer1@ogr.sakarya.edu.tr
+* @since 08.04.23
+* @Messager 
+* Messager stores messages and returns them according to Message headings.
+*/
 package handler.messager;
 
 public class Messager {
@@ -12,23 +19,24 @@ public class Messager {
 			return "No arguments provided, please re-launch for more information with --help or -h parameter";
 
 		case help:
-			return "//\njava -jar test.jar \r\n"
-					+ "java -jar test.jar [-h, --help](optional) : prints to screen possible launch parameters\r\n"
-					+ "java -jar test.jar [--about](optional) : prints to screen readme file content.\r\n"
-					+ "java -jar test.jar [test.java](required) : input file path\r\n"
-					+ "java -jar program.jar [example.java](required) [output path](optional) : path must be in this format -without quotes- \"output\\\\\"\r\n"
-					+ "java -jar program.jar [example.java](required) [-d, --debug](optional) :(debug mode), only gives console output with detailed information.";
+			return "\njava -jar codeparser.jar \r\n"
+					+ "java -jar codeparser.jar [-h, --help](optional) : prints to screen possible launch parameters\r\n"
+					+ "java -jar codeparser.jar [--about](optional) : prints to screen readme file content.\r\n"
+					+ "java -jar codeparser.jar [test.java](required) : input file path\r\n"
+					+ "java -jar codeparser.jar [example.java](required) [output path](optional) : path must be in this format -without quotes- \"output\\\\\"\r\n"
+					+ "java -jar codeparser.jar [example.java](required) [-d, --debug](optional) :(debug mode), only gives console output with detailed information.\r\n"
+					+ "java -jar codeparser.jar [example.java] (required)[-s, --show] (optional : only gives console output as summery of the class.\n)";
 
 		case wrong:
 			return "\nwrong parameters dedected, please run with parameter -h or --help to see how to use\n";
 
 		case about:
-			return "\nIt is a code parser console app/tool. Basicly parses classes.\n"
-					+ "CodeParser designed to scale. in further updates the parser will parse orher languages,\n"
+			return "\nIt is a code parser console app/tool. Basicly parses classes and its components like functions and comments.\n"
+					+ "CodeParser designed to scale. in further updates the parser will parse other languages,\n"
 					+ "the parser will gain more functionality: i'll re-arrange console arguments to obtain input classes\n"
 					+ "wanted components.\n"
 					+ "like: java -jar codeparser.jar example.py [--get funs ~ --get class ~ --get constructor ~ --get libraries]\n"
-					+ "developed by poqob, Mustafa BÝÇER\n" + "github: https://github.com/poqob/analyzer\n"
+					+ "developed by poqob, Mustafa BICER\n" + "github: https://github.com/poqob/analyzer\n"
 					+ "version: 0.1";
 		default:
 			return " ";
