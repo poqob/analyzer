87d7a2bcb110e6b49b8ae17873c0a701a65a6563
Mustafa <mmermmus123@gmail.com>
2023-04-08 06:38:57 +0300

    main language handler was documanted.

--- analyzer/src/handler/languages/LanguageHandler.java
+++ analyzer/src/handler/languages/LanguageHandler.java
@@ -1,3 +1,12 @@
+/**
+*
+* @author Mustafa BÝÇER, mustafa.bicer1@ogr.sakarya.edu.tr
+* @since 08.04.23
+* @LanguageHandler
+* LanguageHandler, i confess this is a bit rushed. but works :D 
+* language handler manages and directs flow of program. 
+* runs program according to given parameters. some of them must be overriden up to entered parameters.
+*/
 package handler.languages;
 
 import handler.extensions.Extensions;
@@ -30,15 +39,30 @@ public class LanguageHandler {
 		}
 	}
 
+	public static void handler(Extensions extension, String path) throws Exception {
+		switch (extension) {
+		case java:
+			_java(path, Mode.show);
+			break;
+		case dart:
+			_dart();
+			break;
+		default:
+			break;
+		}
+	}
+
+	// run java with modes, debug or regular.
 	private static void _java(String path, Mode mode) throws Exception {
 		JavaHandler.handle(path, mode);
 	}
 
+	// run java logic with regular mode but run with given output path.
 	private static void _java(String path, String output) throws Exception {
 		JavaHandler.handle(path, output);
 	}
 
-	// example
+	// example of running other languages
 	private static void _dart() {
 	}
 
