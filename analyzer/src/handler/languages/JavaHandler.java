b041d8d4adff9406dcedb6002a1206f30ad8b844
Mustafa <mmermmus123@gmail.com>
2023-04-08 06:50:40 +0300

    JavaHandler was documanted.

--- analyzer/src/handler/languages/JavaHandler.java
+++ analyzer/src/handler/languages/JavaHandler.java
@@ -1,3 +1,11 @@
+/**
+*
+* @author Mustafa BÝÇER, mustafa.bicer1@ogr.sakarya.edu.tr
+* @since 08.04.23
+* @JavaHandler
+* JavaHandler is a language handler. it handles all situations of run styles for java files.
+* this can be written more accurate to be realistic.
+*/
 package handler.languages;
 
 import java.io.IOException;
@@ -9,47 +17,61 @@ import util.Debug;
 import writer.javaWriter.JavaWriter;
 
 public class JavaHandler {
+	// fields
 	static JavaParser parser;
 	static JavaClass clss;
 	static JavaWriter writer;
 
+	// run with different program modes according to input parameters.
 	public static void handle(String path, Mode mode) throws Exception {
 		_initialize(path);
 		if (mode == Mode.regular)
-			_regular(path);
+			_regular();
 
 		if (mode == Mode.debug)
 			_debug(path);
+
+		if (mode == Mode.show)
+			_onlyShowOnConsole(path);
 	}
 
-	public static void handle(String path, String output) throws Exception {
-		_initialize(path);
-		_regular(path, output);
-	}
-
+	// initializing fields
 	private static void _initialize(String path) throws Exception {
 		parser = new JavaParser(path);
 		clss = parser.parse();
 		writer = new JavaWriter(clss);
 	}
 
-	private static void _regular(String path, String output) throws IOException {
+	// write to given output path and console
+	public static void handle(String path, String output) throws Exception {
+		_initialize(path);
+		_regular(output);
+	}
+
+	// write to console and write comments to wanted location.
+	private static void _regular(String output) throws IOException {
 		writer.writeToConsole();
 		writer.writeToFile(output);
 	}
 
-	private static void _regular(String path) throws IOException {
-		parser = new JavaParser(path);
-		clss = parser.parse();
-		writer = new JavaWriter(clss);
+	// write to console and write comments to execute folder.
+	private static void _regular() throws IOException {
 		writer.writeToConsole();
 		writer.writeToFile();
 	}
 
+	// debug mode shows various informations about class which was read.
 	private static void _debug(String path) {
 		writer.writeToConsole();
 		Debug.Writer(clss.getStatics().concat("\n\n"));
 		Debug.Writer(clss.toString().concat("\n\n"));
 		Debug.Writer(clss.getFunComs());
 	}
+
+	// the difference of this method from _debug is that is shows less information
+	// about class. Summery of class in human lannguage.
+	private static void _onlyShowOnConsole(String path) {
+		writer.writeToConsole();
+	}
+
 }
