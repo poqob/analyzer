f3c8935c8f465bf73cf5c3691012bda588571223
Mustafa <mmermmus123@gmail.com>
2023-04-08 06:40:20 +0300

    Mode was documanted.

--- analyzer/src/handler/program/Mode.java
+++ analyzer/src/handler/program/Mode.java
@@ -1,5 +1,12 @@
+/**
+*
+* @author Mustafa BÝÇER, mustafa.bicer1@ogr.sakarya.edu.tr
+* @since 08.04.23
+* @Mode enum
+* Mode stores Language logics launch style.
+*/
 package handler.program;
 
 public enum Mode {
-	regular, debug
+	regular, debug, show
 }
